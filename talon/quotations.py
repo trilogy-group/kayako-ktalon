ad2b388be695b4ce896a610c7a12d6db963fc80c
Gaurav Mehta <mehta-gaurav@users.noreply.github.com>
2023-10-03 00:41:41 +0530

    Updating regex to avoid trimming email content that has From:, To: etc

--- talon/quotations.py
+++ talon/quotations.py
@@ -135,7 +135,7 @@ RE_ORIGINAL_MESSAGE = re.compile(u'[\s]*[-]+[ ]*({})[ ]*[-]+'.format(
         'Oprindelig meddelelse',
     ))), re.I)
 
-RE_FROM_COLON_OR_DATE_COLON = re.compile(u'((_+\r?\n)?[\s]*:?[*]?({})[\s]?:([^\n$]+\n){{1,2}}){{2,}}'.format(
+RE_FROM_COLON_OR_DATE_COLON = re.compile(u'(((_+\r?\n)?[\s]*:?[*]?({})[\s]?:)([^\n$]+\n){{1,2}}){{2,}}'.format(
     u'|'.join((
         # "From" in different languages.
         'From', 'Van', 'De', 'Von', 'Fra', u'Från',
